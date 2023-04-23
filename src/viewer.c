e156bab671d801bcc35a36b358015e93f63c8ff8
Francisco Casas <franciscojacb@gmail.com>
2023-04-23 17:08:34 -0400

    Handle arguments with argp.

--- src/viewer.c
+++ src/viewer.c
@@ -4,13 +4,79 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <argp.h>
 
+// Screen clear function
 #ifdef _WIN32
 #include <conio.h>
 #else
 #include <stdio.h>
 #define clrscr() printf("\e[1;1H\e[2J")
 #endif
+
+// Program documentation.
+static char doc[] =
+  "3d-ascii-viewer -- an OBJ 3D model format viewer for the terminal";
+
+static char args_doc[] = "INPUT_FILE";
+
+static struct argp_option options[] = {
+  {"width",     'w',    "size",    0,   "Output width in characters" },
+  {"height",    'h',    "size",    0,   "Output height in characters" },
+  { 0 },
+};
+
+struct arguments
+{
+    int surface_width, surface_height;
+    int arg_num;
+    char *input_file;
+};
+
+/* Parse a single option. */
+static error_t parse_opt (int key, char *arg, struct argp_state *state)
+{
+  /* Get the input argument from argp_parse, which we
+     know is a pointer to our arguments structure. */
+  struct arguments *args = state->input;
+
+  switch (key)
+    {
+        case 'w':
+            args->surface_width = atoi(arg);
+            break;
+
+        case 'h':
+            args->surface_height = atoi(arg);
+            break;
+
+        case ARGP_KEY_ARG:
+
+            // Handle too many arguments
+            if (state->arg_num >= 1)
+                argp_usage(state);
+
+            args->input_file = arg;
+
+            state->arg_num++;
+            break;
+
+        case ARGP_KEY_END:
+
+            // Handle not enough arguments
+            if (state->arg_num < 1)
+                argp_usage (state);
+
+            break;
+
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
+}
+
+static struct argp argp = {options, parse_opt, args_doc, doc};
 
 // Translate from the [-1,1]^3 cube to the screen surface.
 vec3 vec3_to_screen(vec3 v)
@@ -39,24 +105,27 @@ vec3 vec3_rotate_x(float cos, float sin, vec3 v)
     return v;
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "usage: %s <input>.obj\n", argv[0]);
-        return 1;
-    }
+    struct arguments args = {0};
 
-    struct model *model = model_load_from_obj(argv[1]);
+    // Default values
+    args.surface_width = 80;
+    args.surface_height = 50;
+
+    argp_parse(&argp, argc, argv, 0, 0, &args);
+
+    struct model *model = model_load_from_obj(args.input_file);
     if (!model)
         return 1;
     model_normalize(model);
 
-    struct surface *surface = surface_init(100, 60);
+    struct surface *surface = surface_init(args.surface_width, args.surface_height);
     if (!surface)
         return 1;
 
-    for (int t = 0; t < 200; t++)
+    int t = 0;
+    while (1)
     {
         float elev = 0.125 * 3.14159265358979323846 * (sinf(0.05 * t) - 1);
         float elev_cos = cosf(elev);
@@ -97,6 +166,7 @@ int main(int argc, char const *argv[])
         clrscr();
         surface_print(stdout, surface);
         usleep(100000);
+        t++;
     }
 
     surface_free(surface);
